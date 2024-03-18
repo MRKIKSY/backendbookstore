639fdd8aeb2a34d0349f55c2d8295ae78d0272c4
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-03-18 16:23:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import { Admin } from './models/Admin.js'
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: ['http://https://frontendbookstore-wm8f.onrender.com'],
+    origin:  ['http://localhost:5173'],
     credentials: true
 }))
 app.use(cookieParser())
